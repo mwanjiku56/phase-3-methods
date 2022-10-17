b4895879578ccca672cfeba46941e33332ac0e39
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-10-17 19:09:04 +0300

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
-# Your code here!
+def greet_programmer 
+puts"Hello, programmer!"
+end
+
+greet_programmer 
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet ("Jimmy")
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default()
+
+def add num1,num2
+    puts num1
+    puts num2 
+    num1+num2
+end
+puts add(2,5)
+
+def halve(number)
+    if number.class != Integer
+    return nil
+end
+number/2
+end
